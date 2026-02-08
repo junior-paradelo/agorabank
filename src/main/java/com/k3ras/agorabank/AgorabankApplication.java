24740b6215f49526da35ec7715d74992d9ef8406
Junior Paradelo <juniorparadelo@gmail.com>
2026-02-08 09:27:40 +0100

    feat: create base project

---
+++ src/main/java/com/k3ras/agorabank/AgorabankApplication.java
@@ -0,0 +1,13 @@
+package com.k3ras.agorabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgorabankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgorabankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/k3ras/agorabank/AgorabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k3ras.agorabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgorabankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
